c8b717b04ea6e6ac3e6cbb77a916b6c2b2389549
Son Tran <tbson87@gmail.com>
2024-12-16 08:25:37 +0700

    Add sample app.

---
+++ backend/src/module/sample/app/intf.go
@@ -0,0 +1,10 @@
+package app
+
+import (
+	"src/common/ctype"
+	"src/module/account/schema"
+)
+
+type TenantRepo interface {
+	Create(data ctype.Dict) (*schema.Tenant, error)
+}
---
+++ backend/src/module/sample/app/srv.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"src/common/ctype"
+	"src/module/account/schema"
+)
+
+type Service struct {
+	tenantRepo TenantRepo
+}
+
+func New(tenantRepo TenantRepo) Service {
+	return Service{tenantRepo}
+}
+
+func (srv Service) Create(data ctype.Dict) (*schema.Tenant, error) {
+	tenant, err := srv.tenantRepo.Create(data)
+	if err != nil {
+		return nil, err
+	}
+	return tenant, nil
+}
