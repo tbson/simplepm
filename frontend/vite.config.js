a4844fa6a6b68372c66eff86f126059cc4c44ac2
Son Tran <tbson87@gmail.com>
2024-12-26 08:28:06 +0700

    Implement react compiler.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,45 @@
 import path from 'path';
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const ReactCompilerConfig = {
+    optimize: true,
+    runtime: 'automatic' // e.g., 'classic' or 'automatic' for JSX runtime
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-        target: "esnext",
+        target: 'esnext'
     },
     resolve: {
         alias: {
-            src: path.resolve(__dirname, "./src"),
-            service: path.resolve(__dirname, "./src/service"),
-            component: path.resolve(__dirname, "./src/component"),
-            style: path.resolve(__dirname, "./src/style"),
+            src: path.resolve(__dirname, './src'),
+            service: path.resolve(__dirname, './src/service'),
+            component: path.resolve(__dirname, './src/component'),
+            style: path.resolve(__dirname, './src/style')
         }
     },
     server: {
-        host: "0.0.0.0",
+        host: '0.0.0.0',
         port: 3000,
         hmr: {
-            host: "simplepm.test",
+            host: 'simplepm.test',
             clientPort: 443,
-            protocol: "wss"
+            protocol: 'wss'
         }
     },
     plugins: [
         react({
-            fastRefresh: process.env.NODE_ENV !== "test"
+            fastRefresh: process.env.NODE_ENV !== 'test',
+            babel: {
+                plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]]
+            }
         })
     ],
     test: {
         globals: true,
-        environment: "jsdom",
-        setupFiles: ["./src/vitest.setup.js"]
+        environment: 'jsdom',
+        setupFiles: ['./src/vitest.setup.js']
     }
 });
