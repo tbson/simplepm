9b596d3ddbb93c77aee4f67d9a25017fca35ed82
Son Tran <tbson87@gmail.com>
2025-02-07 09:58:01 +0700

    Add compatible deps for ant design.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import '@ant-design/v5-patch-for-react-19';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'jotai';
