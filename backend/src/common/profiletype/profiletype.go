4540081312e389a0756a7998f14b070fbe53005b
Son Tran <tbson87@gmail.com>
2025-02-18 20:05:31 +0700

    Add missing tenant role.

--- backend/src/common/profiletype/profiletype.go
+++ backend/src/common/profiletype/profiletype.go
@@ -5,8 +5,9 @@ const ADMIN = "ADMIN"
 const STAFF = "STAFF"
 
 // For customer
+const OWNER = "OWNER"
 const MANAGER = "MANAGER"
 const USER = "USER"
 
 var PlatformProfileTypes = []string{ADMIN, STAFF}
-var TenantProfileTypes = []string{MANAGER, USER}
+var TenantProfileTypes = []string{OWNER, MANAGER, USER}
